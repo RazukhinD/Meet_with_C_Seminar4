0bd6afbdf61df4227fbed1f4fc6b99c8fbf7219a
Dmitrii <razuhindm@gmail.com>
2022-09-26 22:31:13 +0300

    Вторая домашняя задача 4 семинара

---
+++ Task27/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+Clear();
+
+
+WriteLine("Введиде число : ");
+int number = int.Parse(ReadLine()!);
+int sum = SummofNumber(number);
+WriteLine(sum);
+
+
+
+
+
+
+
+
+
+
+int SummofNumber(int number)
+{
+    int result = 0;
+    while (number!=0)
+    {
+        result+=number%10;
+        number=number/10;
+    }
+
+    return result;
+}
