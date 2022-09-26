86d301fe0c203125603abe9d541c3720a80f9fd5
Dmitrii <razuhindm@gmail.com>
2022-09-26 22:21:12 +0300

    Первая домашняя задача 4 семинара

---
+++ Task25/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+Clear();
+
+
+WriteLine("Введиде число А: ");
+int number1 = int.Parse(ReadLine()!);
+WriteLine("Введите число B:");
+int number2 = int.Parse(ReadLine()!);
+int exp = Exponintation(number1,number2);
+WriteLine(exp);
+
+
+
+int Exponintation(int A, int B)
+{
+    int result=1;
+    for (int i=1; i<=B; i++)
+    {
+        result=result*A;
+    }
+    return result;
+}
