3e44e51fb1f3b37b268ef7e25199248c79716995
Dmitrii <razuhindm@gmail.com>
2022-09-26 21:12:03 +0300

    Первая задача 4 семинара

---
+++ Task0/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите число");
+int A = int.Parse(ReadLine()!);
+int sum=GetSumm(A);
+WriteLine($"Сумма числе от 1 до {A} равна:{sum}");
+
+
+int GetSumm(int number)
+{
+    int result = 0;
+    while (number>0)
+    {
+        result+=number;
+        number--;
+    }
+    return result;
+}
